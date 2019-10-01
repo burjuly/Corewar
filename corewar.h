4117abc58ea841f6f8fdb19d4dafb7837d57141d
Draugr scourge Draugr scourge <draudrau@il-b2.21-school.ru>
2019-10-01 16:23:00 +0300

    реализовано добавление игроков на арену, вывод арены, создание списка кареток

--- corewar.h
+++ corewar.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   corewar.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: waddam <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: draudrau <draudrau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/28 18:00:42 by waddam            #+#    #+#             */
-/*   Updated: 2019/10/01 00:42:17 by waddam           ###   ########.fr       */
+/*   Updated: 2019/10/01 16:15:42 by draudrau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ typedef struct		s_plr
 // carriage - каретка
 typedef struct		s_crg
 {
+	int				num_plr;
 	int				pc;
 	int				carry;
 	int				reg[REG_NUMBER];
@@ -40,31 +41,43 @@ typedef struct		s_crg
 	int				bef_op;		// количество циклов, оставшиеся до исполнения операции, на которой стоит каретка
 	int				num_live;	// цикл, в котором в последний раз была выполнена операция live
 	int				step;		// количество байт, которые нужно будет «перешагнуть», чтобы оказаться на следующей операции
+	struct s_crg	*next;
 }					t_crg;
 
 typedef struct		s_cw
 {
 	t_plr			plr[MAX_PLAYERS];
-	t_crg			crg;
-	int				map[MEM_SIZE];
+	t_crg			*crg;
+	char			map[MEM_SIZE];
 	int				plr_nbrs;	// количество игроков
 	int				cclt_die;	// текущий cycle to die
-	int				cur_ccl;	// ноимер текущего цикла
+	int				round;	// количество прошедших с начала игры циклов
 	int				checks;		// количество проверок
 	int				dump;
+
+	int 			count_live; // количество выполненных операций live за последний период, длинной в cycles_to_die
+	int				last_plr; // игрок, о котором в последний раз сказали, что он жив
+
 }					t_cw;
 
-void		ft_leave(char *msg);
-void		ft_initialize(t_cw *cw);
-void		ft_parse(int argc, char **argv, t_cw *cw);
-void		ft_write_plr(char **argv, int *i, t_cw *cw, int pos);
-void		ft_byte_reverse(char *bytes_array);
-void		ft_game(t_cw *cw);
-void		ft_print_map(t_cw *cw);
-void		ft_correct_plrs(t_cw *cw);
-void		ft_sort_plrs(t_plr *plr, int size);
+void				ft_leave(char *msg);
+void				ft_initialize(t_cw *cw);
+void				ft_parse(int argc, char **argv, t_cw *cw);
+void				ft_write_plr(char **argv, int *i, t_cw *cw, int pos);
+void				ft_byte_reverse(char *bytes_array);
+void				ft_game(t_cw *cw);
+void				ft_print_map(t_cw *cw);
+void				ft_correct_plrs(t_cw *cw);
+void				ft_sort_plrs(t_plr *plr, int size);
 
 // debug
-void		ft_print_plrs(t_plr *plr, int size);
+void				ft_print_plrs(t_plr *plr, int size);
+
+// арена
+void				ft_print_map(t_cw *cw);
+void				ft_add_carriage(t_cw *cw, int k, int pc);
+int					ft_search_next_plr(t_cw *cw, int num);
+void				ft_add_plr_on_map(t_cw *cw);
+void				ft_game(t_cw *cw);
 
 #endif
--- cw/init_map.c
+++ cw/init_map.c
@@ -6,7 +6,7 @@
 /*   By: draudrau <draudrau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/29 20:21:44 by draudrau          #+#    #+#             */
-/*   Updated: 2019/09/29 21:35:41 by draudrau         ###   ########.fr       */
+/*   Updated: 2019/10/01 16:16:57 by draudrau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,44 +19,80 @@ void    ft_print_map(t_cw *cw)
     i = 0;
     while (i < 4096)
     {
-        if (i % 64 == 0)
+        if (i % 64 == 0 && i != 0)
             printf("\n");
-        printf("%x", cw->map[i]);
-        // if (i % 2 != 0)
-        //     printf(" ");
+        printf("%.2hhx ", cw->map[i]);
         i++;
     }
 
 }
 
-void    ft_game(t_cw *cw)
+void    ft_add_carriage(t_cw *cw, int k, int pc)
+{
+    t_crg   *start;
+    t_crg   *new;
+
+    start = NULL;
+    if (cw->crg == NULL) // Если создаем 1-ю каретку
+    {
+        if (!(cw->crg = (t_crg *)malloc(sizeof(t_crg))))
+            ft_leave("Придумать что-нибудь на случай, если не выделилась память");
+        cw->crg->num_plr = -k; // кладем номер plr со знаком '-'
+        cw->crg->pc = pc;
+        cw->crg->next = NULL;
+    }
+    else 
+    {
+        start = cw->crg; // запоминаем начало списка
+        if (!(new = (t_crg *)malloc(sizeof(t_crg))))
+            ft_leave("Придумать что-нибудь на случай, если не выделилась память");
+        new->num_plr = -(k + 1); // кладем номер plr со знаком '-'
+        new->pc = pc;
+        new->next = start;
+    }
+}
+
+int    ft_search_next_plr(t_cw *cw, int num)
 {
     int i;
+
+    i = 0;
+    while (i < cw->plr_nbrs)
+    {
+        if (cw->plr[i].num == num)
+            return (i);
+        i++;
+    }
+    return (0);
+}
+
+void    ft_add_plr_on_map(t_cw *cw)
+{
+    int i;
+    int j;
     int k;
     int delta;
 
     i = 0;
     k = 1;
-    cw->plr_nbrs = 2;
-    cw->plr[0].num = 1;
-    cw->plr[1].num = 2;
-    delta = MEM_SIZE / cw->plr_nbrs;
+    delta = MEM_SIZE / cw->plr_nbrs; // отступ между чемпионами
     ft_memset(cw->map, 0, MEM_SIZE);
-    while (1)
+    while (i < 4096)
     {
-        // int j = 0;
-        // while (j < cw->plr[0].code_size)
-        // {  
-        //     printf("%x", (int)cw->plr[0].code[j]);
-        //     j++;
-        // }
-        //printf("%s \n", cw->plr[0].code);
-        if (k <= cw->plr_nbrs)
-            ft_memcpy(cw->map, cw->plr[k].code, cw->plr[k].code_size);
+        j = ft_search_next_plr(cw, k); // Ищем кого будем добавлять на арену следующим
+        if (j < cw->plr_nbrs)
+            ft_memcpy(&(cw->map[i]), cw->plr[j].code, cw->plr[j].code_size);
         else
             break ;
-        i = i + delta;
+        ft_add_carriage(cw, k, i); // Добавляем новую каретку с номером
+        i = i + delta; // Позиция, где начнется код следующего чемпиона
         k++;
     }
+    cw->last_plr = j; // игрок, о котором в последний раз сказали, что он жив
+}
+
+void    ft_game(t_cw *cw)
+{
+    ft_add_plr_on_map(cw);
     ft_print_map(cw);
 }
