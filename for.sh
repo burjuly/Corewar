1a654bdc4b8e0c46e665c7a67743f2e497f649c2
Walda rivers Addam <waddam@il-a2.21-school.ru>
2019-10-20 22:47:04 +0300

    for.sh

--- for.sh
+++ for.sh
@@ -1,11 +1,20 @@
-for (( i=25902; i <= 25903; i++ ))
+MAPS="jumper.cor jumper.cor"
+NUM=24401
+MIN=NUM
+MAX=NUM+2
+# MAX=NUM+2
+for (( i=$MIN; i <= $MAX; i++ ))
 do
-../../corewar_def ../../fluttershy.cor ../../bee_gees.cor  -d $i > ../../their; 
-./corewar ../../fluttershy.cor ../../bee_gees.cor  -dump $i > ../../our;
-diff ../../their ../../our >> ../../diff.diff
+./corewar_def $MAPS -d $i > their;
+./corewar $MAPS -dump $i > our;
+diff their our >> diff.diff
 done
 # for (( i=24688; i <= 24692; i++ ))
 # do
 # ../../corewar_def ../../jumper.cor -d $i > ../../their; ./corewar ../../jumper.cor -dump $i > ../../our; diff ../../their ../../our >> ../../diff.diff
 # done
 # ../../corewar_def ../../jumper.cor ../../zork.cor -d $i > ../../their; ./corewar ../../jumper.cor ../../zork.cor -dump $i > ../../our; diff ../../their ../../our >> ../../diff.diff
+
+# ../../corewar_def ../../fluttershy.cor  -d $i > ../../their;
+# ./corewar ../../fluttershy.cor  -dump $i > ../../our;
+# diff ../../their ../../our >> ../../diff.diff
